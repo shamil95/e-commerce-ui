0cbbdcebac8f9ee34d75fa41e54deb1579290845
shamil isayev <shamil.isayev95@gmail.com>
2022-07-16 12:39:41 +0400

    added layout simple

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
-}
+    return (
+        <>
+            <Header />
+            <main></main>
+            <Footer />
+        </>
+    );
+};
 
 export default App;
